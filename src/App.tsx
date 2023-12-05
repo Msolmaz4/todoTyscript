f599bc1d30e802707ee78542b6d1303f68b1ee02
mustafa <msolmaz83@gmail.com>
2023-12-05 19:47:22 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import InputFeild from './components/InputFeild';
+
+
 
 function App() {
+
+
+  const [todo,setTodo] = useState<string>('')
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='heading'> TASKIFY</span>
+      <InputFeild todo = {todo} setTodo = {setTodo}/>
+    
     </div>
   );
 }
---
+++ src/Not.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+// let name:string;
+// let age:number | string;
+// let isStudent:boolean;
+// let hobbies:string[]
+// let role:[number,string]
+// role=[5,'mustafada']
+ 
+// type Person ={
+//   name:string,
+//   age?:number,
+// }
+
+// let person :Person={
+//   name:'ahmet',
+//   age:25
+// }
+// let lotofPeople:Person[];
+
+// let printName:Function,
+// let printName:(name:string)=>void,
+
+// let Person:unknown,
+// function printName (name:string){
+//   console.log(name)
+// }
+// printName('adana')
+// name='mustafaa'
+const Not = () => {
+  return (
+    <div>Not</div>
+  )
+}
+
+export default Not
