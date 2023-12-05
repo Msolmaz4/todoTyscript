7b6a3fd6e8c42dbd68a6b82a147422bed43b63dd
mustafa <msolmaz83@gmail.com>
2023-12-05 23:41:13 +0100

    edit

--- src/components/Single.tsx
+++ src/components/Single.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
 import { IoMdDoneAll } from "react-icons/io";
@@ -15,6 +15,8 @@ interface Props {
 
 
 const Single= ({todo,todos,setTodos}:Props) => {
+    const [edit,setEdit] = useState<boolean>(false)
+    const [edittodo,setEditTodo] = useState<string>(todo.todo)
 
 const handleDone = (id:number)=>{
     setTodos(todos.map((item)=>item.id === id ?{...todo,isDone:!todo.isDone} : todo))
@@ -24,10 +26,25 @@ const handleDelete = (id:number)=>{
     setTodos(todos.filter((item)=>item.id !== id))
 
 }
-
+const handleEdit = (e:React.FormEvent,id:number)=>{
+    e.preventDefault()
+    setTodos(todos.map((todo)=>(todo.id === id ? {...todo,todo:edittodo} : todo)))
+    setEdit(false)
+}
   return (
     <div>
-        <form action="" className='todos_single'>
+        <form action="" className='todos_single' onSubmit={(e)=>handleEdit(e,todo.id)}>
+            {
+                edit ? (<input value={edittodo} onChange={(e)=>setEditTodo(e.target.value)}/>) :(todo.isDone ? ( 
+                    // burda s ihtar etikedig bir sey olabilir arastir
+                    <s className='todos_single--text'>
+                    {todo.todo}
+                </s>):(<span className='todos_single--text'>
+                    {todo.todo}
+                </span>)
+
+                )
+            }
 
             {todo.isDone ? ( 
                 // burda s ihtar etikedig bir sey olabilir arastir
@@ -39,7 +56,11 @@ const handleDelete = (id:number)=>{
 
           
             <div>
-               <span className='icon' onClick={()=>handleEdit(todo.id)}><CiEdit /></span> 
+               <span className='icon' onClick={()=>{
+                if(!edit && !todo.isDone){
+                    setEdit(!edit)
+                }
+               }}><CiEdit /></span> 
                <span className='icon' onClick={()=>handleDelete(todo.id)}><MdDelete /></span> 
                <span className='icon' onClick={()=>handleDone(todo.id)}><IoMdDoneAll /></span> 
             </div>
